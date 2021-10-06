6feeb0e51d40a6d5227e10030551508ff4e40cb2
dhivya-ganesh <91806561+dhivya-ganesh@users.noreply.github.com>
2021-10-06 15:19:38 +0200

    App cross origin

--- src/main/java/com/montyhallsimulation/SimulatorApp.java
+++ src/main/java/com/montyhallsimulation/SimulatorApp.java
@@ -5,6 +5,7 @@ import java.util.Random;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,6 +17,7 @@ public class SimulatorApp {
 	// random object to auto assign doors to player, prize and presenter
 	public static final Random autoDoorAssigner = new Random();
 
+	@CrossOrigin(origins = "http://localhost:4200")
 	@GetMapping("/simulate/montyhall/{noOfSimulations}/{switchOrStick}")
 	@ResponseBody
 	String simulator(@PathVariable("noOfSimulations") Integer simulations,
